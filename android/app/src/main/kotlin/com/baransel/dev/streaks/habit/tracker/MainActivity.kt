81eb52b2604727be4c8962dde8c6c96a9c99c99c
kaex <root@baransel.dev>
2025-03-29 01:06:42 +0200

    Update Android package name to com.baransel.dev.streaks.habit.tracker

---
+++ android/app/src/main/kotlin/com/baransel/dev/streaks/habit/tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.baransel.dev.streaks.habit.tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/streaks/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.streaks
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
